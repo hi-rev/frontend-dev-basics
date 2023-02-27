1911f8ebc4c9138eeaa753c37116a636e9d1f0d9
hi-rev <hirev05@gmail.com>
2023-02-27 23:22:35 +0900

    과제 예시 코드 추가

--- ch07/src/main/webapp/image-viewer.js
+++ ch07/src/main/webapp/image-viewer.js
@@ -1,5 +1,81 @@
 imageViewer = {
 	init: function() {
+		$(function() {
+			$("#btn-change").click(this._changeImage.bind(this));
+			$("#btn-slideshow").click(this._slideShow.bind(this));
+			
+			// 초기 이미지 선택
+			this._changeImage();
+		}.bind(this));
+	},
+	
+	_changeImage: function() { // 이미지 변경 버튼 눌렀을 때 동작
+		do {
+				var index = Math.floor(Math.random()*this._images.length); // 랜덤 _images 배열 인덱스값
+				var info = this._images[index]; // 랜덤 이미지 정보
+		} while ($("img").attr("title") === info.name);
+		
+		$("img").attr({
+			src: "images/" + info.file,
+			title: info.name
+		});
+	},
+	
+	_slideShow: function() { // 슬라이드쇼 버튼 눌렀을 때 동작
+		if (this._intervalId) {
+			// 슬라이드 쇼 진행 중
+			
+			// 1. 타이머 정지
+			clearInterval(this._intervalId);
+			this._intervalId = null;
+			
+			// 2. 버튼 변경
+			$("#btn-slideshow").text("슬라이드 쇼 시작");
+		} else {
+			// 슬라이드 쇼 멈춘 상태
+			
+			// 1. 타이머 시작
+			this._intervalId = setInterval(function() {
+				this._changeImage();
+			}.bind(this), 1000);
+			
+			// 2. 버튼 변경
+			$("#btn-slideshow").text("슬라이드 쇼 중지");
+		}
+	},
+	
+	_intervalId: null,
+	
+	_images: [{
+		name: '국화', 
+		file: 'Chrysanthemum.jpg'
+	}, {
+		name: '사막', 
+		file: 'Desert.jpg'
+	}, {
+		name: '수국', 
+		file: 'Hydrangeas.jpg'
+	}, {
+		name: '젤리피쉬', 
+		file: 'Jellyfish.jpg'
+	}, {
+		name: '코알라', 
+		file: 'Koala.jpg'
+	}, {
+		name: '등대', 
+		file: 'Lighthouse.jpg'
+	}, {
+		name: '펭귄', 
+		file: 'Penguins.jpg'
+	}, {
+		name: '튤립', 
+		file: 'Tulips.jpg'
+	}]
+}
+
+
+/* imageViewer = {
+	init: function() {
 		var index = this._changeImage();
 		return index;
 	},
@@ -93,4 +169,4 @@ $(function() {
 			clearInterval(interval);
 		}
 	});
-});
+}); */
